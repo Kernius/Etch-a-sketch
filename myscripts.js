7655324d3aa5331291872c7c7d978a7129929a6a
Kernius Kaladinskas <kernius1@gmail.com>
2022-11-17 18:47:41 -0500

    toggling painting of the divs and color panel for color choice are done, starting function with a click and stopping it with a second click done

--- myscripts.js
+++ myscripts.js
@@ -1,44 +1,83 @@
-function createSketch () {
-    let container = document.querySelector('#container');
+let container = document.querySelector('#container');
+let isPainting = false;
 
-    for(i = 0; i < 256; i++) {
+/* Default sketch creation */
+
+function createSketch() {
+    for(let i = 0; i < 256; i++) {
         let boxes = document.createElement('div');
-        boxes.style.cssText = "border: 1px solid black; height: 30px; width: 30px;"
+        boxes.classList.add('box');
         container.appendChild(boxes);
     }
-}
+};
 
 createSketch ();
 
+/* function to remove all boxes when using a slider */
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/* slider to change size of the sketch */
+
+    let slider = document.querySelector('#slider');
+    const sliderVal = document.querySelector('.value');
+
+    slider.addEventListener('input', function() {
+        sliderVal.textContent = slider.value;
+        removeAllChildNodes(container);
+        container.setAttribute('style', `grid-template-columns: repeat(${slider.value}, 1fr); grid-template-rows: repeat(${slider.value}, 1fr);`);
+        for (let i = 0; i < slider.value*slider.value; i++) {
+            const div = document.createElement('div');
+            div.classList.add('box');
+            container.appendChild(div);
+        }
+        });
+
+/* toggling painting on and off*/
+
+container.addEventListener('click', togglePaint);
+  
 
 
-function paintBoxes () {
+function togglePaint () {
     
     const gridBoxes = document.querySelectorAll('#container > div');
-    gridBoxes.forEach((item) => { 
-    item.addEventListener('click', () => {
-    gridBoxes.forEach((item) => { 
-        item.addEventListener('mouseover', () => {
-            let color = document.querySelector('#color').value;
-            item.style.backgroundColor = color;
+
+    if(!isPainting) {
+        gridBoxes.forEach((item) => { 
+            item.addEventListener('mouseover', paint); 
+        });
+        isPainting = true;
+    } else { 
+        gridBoxes.forEach((item) => {
+            item.removeEventListener('mouseover', paint);
+        });
+        
+        isPainting = false;
+    }
+}
+
+/* color panel and coloring function*/
 
 
-
-    });
-    });
-    });
-    });
-    
-
+function paint(e) {
+        let color = document.querySelector('#color').value;
+        e.target.style.backgroundColor = color;
 }
 
 
 
 
 
+
+
+
     
 
 
-paintBoxes ();
 
 
